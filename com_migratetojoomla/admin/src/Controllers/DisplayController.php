325fc0040c861085d6d34dff4e104582070c9862
Kaushik1216 <kaushik.vishwakarma2003@gmail.com>
2023-07-10 20:01:24 +0530

    Initial component setup

---
+++ com_migratetojoomla/admin/src/Controllers/DisplayController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Joomla\Component\MigrateToJoomla\Administrator\Controller;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\Controller\BaseController;
+
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_migratetojoomla
+ *
+ * @copyright   
+ * @license     GNU General Public License version 3; see LICENSE
+ */
+
+/**
+ * Default Controller of MigrateToJoomla component
+ *
+ * @package     Joomla.Administrator
+ * @subpackage  com_migratetojoomla
+ */
+class DisplayController extends BaseController {
+    /**
+     * The default view for the display method.
+     *
+     * @var string
+     */
+    protected $default_view = 'main';
+    
+    public function display($cachable = false, $urlparams = array()) {
+        return parent::display($cachable, $urlparams);
+    }
+    
+}
---
+++ com_migratetojoomla/admin/src/View/Main/HtmlView.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Joomla\Component\MigrateToJoomla\Administrator\View\Main;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_migratetojoomla
+ *
+ * @copyright   
+ * @license     GNU General Public License version 3; see LICENSE
+ */
+
+/**
+ * Main "Migrate To Joomla" Admin View
+ */
+class HtmlView extends BaseHtmlView {
+    
+    /**
+     * Display the main "Migrate To Joomla" view
+     *
+     * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+     * @return  void
+     */
+    function display($tpl = null) {
+        parent::display($tpl);
+    }
+
+}
