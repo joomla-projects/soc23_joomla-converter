16575c385f814b52936fc885ddf59030238ba799
Kaushik1216 <kaushik.vishwakarma2003@gmail.com>
2023-07-10 23:04:42 +0530

    change lanuguage file

--- com_migratetojoomla/admin/src/Controllers/DisplayController.php
+++ com_migratetojoomla/admin/src/Controllers/DisplayController.php
@@ -26,10 +26,22 @@ class DisplayController extends BaseController
      * The default view for the display method.
      *
      * @var string
+     * @since  1.6
      */
     protected $default_view = 'main';
-    
-    public function display($cachable = false, $urlparams = array()) {
+
+    /**
+     * Method to display a view.
+     *
+     * @param   boolean  $cachable   If true, the view output will be cached
+     * @param   array    $urlparams  An array of safe URL parameters and their variable types, for valid values see {@link \JFilterInput::clean()}.
+     *
+     * @return  BaseController|boolean  This object to support chaining.
+     *
+     * @since   1.5
+     */
+
+    public function display($cachable = false, $urlparams = []) {
         return parent::display($cachable, $urlparams);
     }
     
