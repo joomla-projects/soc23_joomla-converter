634ca46a6edd3d6801305248264cce07b15da4ee
Kaushik1216 <kaushik.vishwakarma2003@gmail.com>
2023-07-15 16:13:11 +0530

    Added assets

--- com_migratetojoomla/admin/src/controller/HttpController.php
+++ com_migratetojoomla/admin/src/controller/HttpController.php
@@ -8,19 +8,26 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
+namespace Joomla\Component\MigrateToJoomla\Administrator\HttpController;
 
-namespace Joomla\Component\MigrateToJoomla\Administrator\Helper;
- 
+use Joomla\CMS\MVC\Controller\FormController;
+use Joomla\CMS\Router\Route;
 use Joomla\CMS\Factory;
-use Joomla\CMS\Language\Text;
 use Joomla\CMS\Http\HttpFactory;
+use Joomla\CMS\Versioning\VersionableControllerTrait;
+
 // phpcs:disable PSR1.Files.SideEffects
 \defined('_JEXEC') or die;
 // phpcs:enable PSR1.Files.SideEffects
- 
 
-class HttpHelper
-{	
+/**
+ * main controller class.
+ *
+ * @since  
+ */
+class HttpController extends FormController
+{
+    use VersionableControllerTrait;
     /**
      * Method to check Enter http url connection
      * 
@@ -28,23 +35,24 @@ class HttpHelper
      * 
      * since
      */
-    public static function testhttpconnection($url = NULL) {
+    public function testhttpconnection($url = NULL) {
         
         $app   = Factory::getApplication();
 
         $http = HttpFactory::getHttp();
         
-        $response = $http->head($url);
+        $response = $http->get($url);
 
         $statusCode = $response->code;
         
         if($statusCode==200) {
-           $app->enqueueMessage(TEXT::_('COM_MIGRATETOJOOMLA_HTTP_CONNECTION_SUCCESSFULLY') , 'success');
+            $app->enqueueMessage("Http Connection Succesesfully" , 'success');
         }else{
-           $$app->enqueueMessage(TEXT::_('COM_MIGRATETOJOOMLA_HTTP_CONNECTION_UNSUCCESSFULLY'), 'warning');
+            $app->enqueueMessage("Http Connection Unsuccesesfully" , 'warning');
         }
 
         return ($statusCode==200);
+
     }
     
-}?>
+}
--- com_migratetojoomla/admin/tmpl/main/default.php
+++ com_migratetojoomla/admin/tmpl/main/default.php
@@ -8,19 +8,26 @@
  * @license       GNU General Public License version 2 or later; see LICENSE.txt
  */
 
+use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Router\Route;
-
- // No direct access to this file
+// No direct access to this file
 defined('_JEXEC') or die('Restricted Access');
+/** @var \Joomla\Component\Banners\Administrator\View\Banner\HtmlView $this */
 
+/** @var Joomla\CMS\WebAsset\WebAssetManager $wa */
+$wa = $this->document->getWebAssetManager();
+$wa->useScript('com_migratetojoomla.admin-migratetojoomla');
+
+// $document = Factory::getDocument();
+// $document->addScript('/media/migratetojoomla/js/admin-migratetojoomla.js');
 
 ?>
 <style>
-    #migratetojoomla{
+    /* #migratetojoomla{
         background-color: #F8FAFC;
-    }
+    } */
 </style>
 <div id="migratetojoomla" class="p-2">
     <h3 class="mt-2"><?php echo Text::_('COM_MIGRATETOJOOMLA_WORDPRESS_WEBSITE_PARAMETERS')?></h3>
@@ -29,7 +36,7 @@ defined('_JEXEC') or die('Restricted Access');
 
 	<?php echo $this->form->renderField('livewebsiteurl');  ?>
 
-	<button type="button" class="btn btn-primary" onclick="Joomla.submitbutton('main.checkconnection')">Check Connection</button>
+	<button type="button" id="migratetojoomlahttp"class="btn btn-primary" >Check Connection</button>
 
     <input type="hidden" name="task" value="">
     <?php echo HTMLHelper::_('form.token'); ?>
