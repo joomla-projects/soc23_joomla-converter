2e9b5c8f1a1b9ed12ddd7fdd8caea9823cc0d767
BrianTeeman <brian@teeman.net>
2024-12-02 23:32:42 +0000

    package/subpackage headers

--- src/plugins/migratetojoomla/wordpress/src/Extension/Wordpress.php
+++ src/plugins/migratetojoomla/wordpress/src/Extension/Wordpress.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- * @package     Joomla.Administrator
- * @subpackage  com_migratetojoomla.wordpress
+ * @package     Joomla.Plugin
+ * @subpackage  Migratetojoomla.wordpress
  *
  * @copyright   (C) 2024 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
